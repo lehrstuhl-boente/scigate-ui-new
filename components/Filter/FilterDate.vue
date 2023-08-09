42238bab8e7657e502a132bb06476f5cebf8eafa
Atomis14 <michelsabbatini@outlook.com>
2023-08-09 14:24:06 +0200

    #2 keep date values after page reload

--- components/Filter/FilterDate.vue
+++ components/Filter/FilterDate.vue
@@ -2,15 +2,26 @@
   <div class="flex items-center">
     <label class="mr-3">
       <span class="mr-2">{{ $t('from') }}:</span>
-      <input type="number" placeholder="1990" class="py-1 px-2 max-w-[80px] rounded-md border">
+      <input type="number" v-model="from" placeholder="1990" class="py-1 px-2 max-w-[80px] rounded-md border">
     </label>
     <label>
       <span class="mr-2">{{ $t('to') }}:</span>
-      <input type="number" placeholder="2000" class="py-1 px-2 max-w-[80px] rounded-md border">
+      <input type="number" v-model="to" placeholder="2000" class="py-1 px-2 max-w-[80px] rounded-md border">
     </label>
   </div>
 </template>
 
 <script lang="ts" setup>
 const { filter } = defineProps<{ filter: FilterDate }>();
+
+const from = ref(localStorage.getItem('filter.date.from'));
+const to = ref(localStorage.getItem('filter.date.to'));
+
+// save state of the two number fields to localstorage in order to preserve it after page refresh
+watch(from, (newValue, oldValue) => {
+  if (newValue != null) localStorage.setItem('filter.date.from', newValue);
+});
+watch(to, (newValue, oldValue) => {
+  if (newValue != null) localStorage.setItem('filter.date.to', newValue);
+});
 </script>
