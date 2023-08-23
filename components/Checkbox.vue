217c873f6e1ceb1724fa58c5567611cf73827ac2
Atomis14 <michelsabbatini@outlook.com>
2023-08-23 11:28:17 +0200

    change from option name from "noAssignment" to "unknown"

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -2,8 +2,8 @@
   <label class="flex items-center hover:bg-black/5 px-1 rounded" :name="option">
     <input type="checkbox" class="mr-2" v-model="option.checked">
     <div class="flex justify-between items-center w-full">
-      <span v-if="option.name == 'noAssignment'">
-        {{ $t('noAssignment') }}
+      <span v-if="option.name == 'unknown'">
+        {{ $t('unknown') }}
       </span>
       <span v-else>
         {{ $t(`filters.${filterId}.${option.name}`) }}
