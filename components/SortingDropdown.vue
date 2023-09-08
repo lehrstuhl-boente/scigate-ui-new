b17700b6f97a5adc70e4056dc65a13d0e85ae32d
Atomis14 <michelsabbatini@outlook.com>
2023-09-08 10:01:03 +0200

    add value to fcfs option

--- components/SortingDropdown.vue
+++ components/SortingDropdown.vue
@@ -2,7 +2,7 @@
   <div>
     <label class="mr-3 text-sm">{{ $t('sorting') }}</label>
     <select class="bg-white px-2 py-1 border rounded-md text-sm">
-      <option value="">First Come First Serve</option>
+      <option value="fcfs">First Come First Serve</option>
     </select>
   </div>
 </template>
