c31de0e18d69f4fb205259ff010f6db9ac7a7481
Atomis14 <michelsabbatini@outlook.com>
2023-10-19 13:54:45 +0200

    remove images from start page

--- components/EngineCard.vue
+++ components/EngineCard.vue
@@ -2,13 +2,13 @@
   <a :href="engine.url" target="_blank"
     class="bg-white shadow-lg rounded-xl hover:shadow-xl hover:cursor-pointer hover:scale-105 active:scale-95"
     :title="engine.title">
-    <div class="p-3">
+    <!-- <div class="p-3">
       <div class="h-[50px] grid place-items-center">
         <img :src="`/engines/${engine.id}.${engine.imageFormat}`" :alt="`${engine.title} logo`" class="w-3/4"
           :class="{ 'h-[50px]': engine.imageFormat == 'svg' }">
       </div>
-    </div>
-    <div class="px-4 pb-3 items-center text-center">
+    </div> -->
+    <div class="px-4 py-3 items-center text-center">
       <div class="line-clamp-1">{{ engine.title }}</div>
     </div>
   </a>
